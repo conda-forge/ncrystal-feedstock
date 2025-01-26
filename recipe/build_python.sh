05f77b79486b0c8b729a4f783352b3bb5bf80da0
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-26 09:36:35 +0100

    More test printouts

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 set -eux
 test -f "${SRC_DIR}/src/ncrystal_python/pyproject.toml"
-${PYTHON} -m pip install "${SRC_DIR}/src/ncrystal_python" -vv --no-deps --no-build-isolation
+
+echo "TKTEST DOLLAR-PYTHON: ${PYTHON:-undefined}"
+echo "TKTEST DOLLAR-CONDA_PREFIX: ${CONDA_PREFIX:-undefined}"
+echo "TKTEST ls DOLLAR-CONDA_PREFIX/bin:"
+ls -l ${CONDA_PREFIX:-undefined}/bin
+echo "TKTEST which python"
+which python
+
+#was ${PYTHON} -m pip install "${SRC_DIR}/src/ncrystal_python" -vv --no-deps --no-build-isolation
+pip install "${SRC_DIR}/src/ncrystal_python" -vv --no-deps --no-build-isolation
+
+${PYTHON} -c 'print("TKTEST hello")'
