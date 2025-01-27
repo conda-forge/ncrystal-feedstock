4c926c435605dc38cfe2e3cc00f59cc6e1d3c3fb
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-27 08:35:27 +0100

    Correct CMAKE_ARGS placement

--- recipe/build_core.bat
+++ recipe/build_core.bat
@@ -14,12 +14,12 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cmake ^
     -B %BLDDIR% ^
     -S %CD%\src\ncrystal_core ^
+    %CMAKE_ARGS% ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DNCRYSTAL_ENABLE_DATA=EMBED ^
     -DNCRYSTAL_NOTOUCH_CMAKE_BUILD_TYPE=ON ^
-    -DNCRYSTAL_MODIFY_RPATH=OFF ^
-    %CMAKE_ARGS%
+    -DNCRYSTAL_MODIFY_RPATH=OFF
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
--- recipe/build_core.sh
+++ recipe/build_core.sh
@@ -9,13 +9,13 @@ test -f "${SRC_DIR}/src/ncrystal_core/include/NCrystal/NCrystal.hh"
 cmake \
     -B ./build_core \
     -S "${SRC_DIR}/src/ncrystal_core" \
+    ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=Release \
     -DNCRYSTAL_ENABLE_DATA=EMBED \
     -DNCRYSTAL_NOTOUCH_CMAKE_BUILD_TYPE=ON \
-    -DNCRYSTAL_MODIFY_RPATH=OFF \
-    ${CMAKE_ARGS}
+    -DNCRYSTAL_MODIFY_RPATH=OFF
 
 cmake --build ./build_core --config Release --parallel ${CPU_COUNT:-2}
 cmake --install ./build_core
