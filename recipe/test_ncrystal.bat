fb9aa9c73df7c90b2c7d423e73fdc2441e25cf7a
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-28 14:08:33 +0100

    update src

--- recipe/test_ncrystal.bat
+++ recipe/test_ncrystal.bat
@@ -25,7 +25,12 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 @REM fixme add --fail-if-devel to next line once out of development
 %PYTHON% %CD%\src\devel\bin\ncdevtool verifytag -t "%PKG_VERSION%" -p 'X.Y.Z' --file-verify=VERSION
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 %PYTHON% %CD%\src\devel\bin\ncdevtool check -n fixme
+
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 ncrystal-config --help
 
@@ -90,23 +95,37 @@ cmake --find-package -DNAME=NCrystal -DCOMPILER_ID=GNU -DLANGUAGE=CXX -DMODE=EXI
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 %PYTHON% -m pip install %CD%\src\ncrystal_pypluginmgr -vv --no-deps --no-build-isolation
+if %errorlevel% neq 0 exit /b %errorlevel%
 %PYTHON% -m pip check
+if %errorlevel% neq 0 exit /b %errorlevel%
 %PYTHON% -m pip install %CD%\src\examples\plugin -vv --no-deps --no-build-isolation
+if %errorlevel% neq 0 exit /b %errorlevel%
 %PYTHON% -m pip check
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 if NOT EXIST %CD%\src\ncrystal_pypluginmgr\pyproject.toml exit /b 1
+
 if NOT EXIST %CD%\src\examples\plugin\pyproject.toml exit /b 1
 
 set "NCRYSTAL_PLUGIN_RUNTESTS=1"
+if %errorlevel% neq 0 exit /b %errorlevel%
 set "NCRYSTAL_REQUIRED_PLUGINS=DummyPlugin"
+if %errorlevel% neq 0 exit /b %errorlevel%
 nctool --plugins
+if %errorlevel% neq 0 exit /b %errorlevel%
 set "NCRYSTAL_PLUGIN_RUNTESTS="
+if %errorlevel% neq 0 exit /b %errorlevel%
 set "NCRYSTAL_REQUIRED_PLUGINS="
+if %errorlevel% neq 0 exit /b %errorlevel%
 nctool -d "plugins::DummyPlugin/somefile.ncmat"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 %PYTHON% -m pip install %CD%\src\examples\plugin_dataonly -vv --no-deps --no-build-isolation
+if %errorlevel% neq 0 exit /b %errorlevel%
 %PYTHON% -m pip check
+if %errorlevel% neq 0 exit /b %errorlevel%
 nctool -d "plugins::DummyDataPlugin/dummy.ncmat"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 @REM fixme stuff from ncrystal windows CI
 @REM fixme downstream cmake project
